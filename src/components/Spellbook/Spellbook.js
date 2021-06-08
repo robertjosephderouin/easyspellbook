fee578a699fbbac7cc5f6620a82feaed464885d4
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-08 10:59:40 -0700

    Add basic dashboard info

---
+++ src/components/Spellbook/Spellbook.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Spellbook.css';
+
+const Spellbook = () => {
+  return(
+    <section>
+      <h1>Easy Spell Book</h1>
+    </section>
+  )
+}
+
+export default Spellbook;
