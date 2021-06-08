3d5359d587438565ec4145de4b24ecb2001fb6f4
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-08 12:07:54 -0700

    Add more details to spotlight text

--- src/components/Spotlight/Spotlight.js
+++ src/components/Spotlight/Spotlight.js
@@ -36,6 +36,13 @@ class Spotlight extends Component {
         <article key={this.state.spotLight?.url}>
           <h2>{this.state.spotLight?.name}</h2>
           <p>{this.state.spotLight?.desc}</p>
+          <p>{this.state.spotLight?.higher_level}</p>
+          <p>Range: {this.state.spotLight?.range}</p>
+          <p>Material: {this.state.spotLight?.material}</p>
+          <p>Duration: {this.state.spotLight?.duration}</p>
+          <p>Casting Time: {this.state.spotLight?.casting_time}</p>
+          <p>Level: {this.state.spotLight?.level}</p>
+          <p>School: {this.state.spotLight?.school.name}</p>
           <Link to={'/'}>🔙</Link>
         </article>
       </div>
