d9408b59d43f84910684fb3ca981ce45d1406bb6
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-08 10:58:39 -0700

    Add basic structure to spells

---
+++ src/components/Spells/Spells.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from '../Card/Card';
+import './Spells.css';
+import { Link } from 'react-router-dom';
+
+const Spells = ({spells}) => {
+
+  const spellCards = spells.map(spell => {
+    return (
+      <Link to={`/${spell.url}`} key={spell.url}>
+        <Card
+          name={spell.name}
+          index={spell.index}
+          url={spell.url)}
+        />
+      </Link>
+    )
+  })
+
+  return (
+    <div>
+      {spellCards}
+    </div>
+  )
+}
+
+
+export default Spells;
