9044f13205c778da2ccb86e2396fc0997f69285b
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-08 10:49:22 -0700

    Add get spotlight fetch

--- src/api.js
+++ src/api.js
@@ -3,3 +3,8 @@ export const getSpells = () => {
     return fetch('https://www.dnd5eapi.co/api/spells')
       .then(response => response.json())
   }
+
+  export const getSpotlight = (url) => {
+    return fetch(`https://www.dnd5eapi.co/api/spells/${url}`)
+      .then(response => response.json())
+  }
