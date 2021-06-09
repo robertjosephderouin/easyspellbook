ff2989d6e2c6168054e7a913773a0c3289384224
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-09 15:47:26 -0700

    Add count

--- src/components/Spells/Spells.js
+++ src/components/Spells/Spells.js
@@ -3,14 +3,16 @@ import Card from '../Card/Card';
 import './Spells.css';
 import { Link } from 'react-router-dom';
 
-const Spells = ({spells, findSpell, toggleSpell}) => {
+const Spells = ({spells, findSpell, memorizeSpell, unmemorizeSpell, countSpell}) => {
 
   const spellCards = spells.map(spell => {
     return (
       <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to={`${spell.index}`} key={spell.url}>
         <article className="card">
             <Card name={spell.name}/>
-            <button onClick={(e) => {e.preventDefault(); toggleSpell(spell.name)}}>{findSpell(spell.name) ? "Unmemorize Spell" : "Memorize Spell"}</button>
+            <p>{countSpell(spell.name)}</p>
+            <button onClick={(e) => {e.preventDefault(); memorizeSpell(spell.name)}}>Memorize Spell</button>
+            <button onClick={(e) => {e.preventDefault(); unmemorizeSpell(spell.name)}}>Unmemorize Spell</button>
         </article>
        </Link>
     )
