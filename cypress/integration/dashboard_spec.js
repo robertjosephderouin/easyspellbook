221a81ec3e752ee0c774a7a35ba53527a761e3bf
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-11 13:33:26 -0700

    Add basic dash board testing

---
+++ cypress/integration/dashboard_spec.js
@@ -0,0 +1,46 @@
+describe('Dashboard', () => {
+
+    it('it should display the data provided as spell cards', () => {
+      const baseURL = 'https://www.dnd5eapi.co/api/spells';
+      cy.intercept('GET', `${baseURL}`, {
+        results: [
+            {
+                "index": "acid-arrow",
+                "name": "Acid Arrow",
+                "url": "/api/spells/acid-arrow"
+            },
+            {
+                "index": "acid-splash",
+                "name": "Acid Splash",
+                "url": "/api/spells/acid-splash"
+            },
+            {
+                "index": "aid",
+                "name": "Aid",
+                "url": "/api/spells/aid"
+            },
+            {
+                "index": "alarm",
+                "name": "Alarm",
+                "url": "/api/spells/alarm"
+            },
+      ]
+      })
+      cy.visit('http://localhost:3000/')
+        .get('article').contains('Acid Arrow')
+  
+    });
+  
+    it('it should display Loading if no spells have yet been retrieved', () => {
+      cy.visit('http://localhost:3000/')
+        .get('h2').contains('Loading')
+    });
+  
+    it('it should display an error of something went wrong when something goes wrong with the host', () => {
+      const baseURL = 'https://www.dnd5eapi.co/api/spells';
+      cy.intercept('GET', `${baseURL}`, {})
+        .visit('http://localhost:3000/')
+        .get('h3').contains('Something went wrong')
+    });
+  
+  });
