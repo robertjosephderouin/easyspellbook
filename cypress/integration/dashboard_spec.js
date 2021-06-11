7b889d57adda8cf4927091a15172ccdd7543652f
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-11 13:59:06 -0700

    Add cypress testing of memorize spell

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -30,6 +30,69 @@ describe('Dashboard', () => {
         .get('article').contains('Acid Arrow')
   
     });
+
+    it('it should display different data provided as spell cards', () => {
+        const baseURL = 'https://www.dnd5eapi.co/api/spells';
+        cy.intercept('GET', `${baseURL}`, {
+          results: [
+              {
+                  "index": "acid-arrow",
+                  "name": "Acid Arrow",
+                  "url": "/api/spells/acid-arrow"
+              },
+              {
+                  "index": "acid-splash",
+                  "name": "Acid Splash",
+                  "url": "/api/spells/acid-splash"
+              },
+              {
+                  "index": "aid",
+                  "name": "Aid",
+                  "url": "/api/spells/aid"
+              },
+              {
+                  "index": "alarm",
+                  "name": "Alarm",
+                  "url": "/api/spells/alarm"
+              },
+        ]
+        })
+        cy.visit('http://localhost:3000/')
+          .get('article').contains('Aid')
+    
+      });
+
+      it('it should display the memorize count as one higher when you click memorize spell', () => {
+        const baseURL = 'https://www.dnd5eapi.co/api/spells';
+        cy.intercept('GET', `${baseURL}`, {
+          results: [
+              {
+                  "index": "acid-arrow",
+                  "name": "Acid Arrow",
+                  "url": "/api/spells/acid-arrow"
+              },
+              {
+                  "index": "acid-splash",
+                  "name": "Acid Splash",
+                  "url": "/api/spells/acid-splash"
+              },
+              {
+                  "index": "aid",
+                  "name": "Aid",
+                  "url": "/api/spells/aid"
+              },
+              {
+                  "index": "alarm",
+                  "name": "Alarm",
+                  "url": "/api/spells/alarm"
+              },
+        ]
+        })
+        cy.visit('http://localhost:3000/')
+          .get('button').contains('Memorize Spell').click()
+          .get('p').contains('Memorized : 1')
+    
+      });
   
     it('it should display Loading if no spells have yet been retrieved', () => {
       cy.visit('http://localhost:3000/')
